6eb71fd9981d895e97744ad5a54ad62d362319e9
kingscott <me@kingscott.ca>
2024-05-14 12:44:05 -0400

    Initial commit for Mitchell H's working vm config

---
+++ flake.nix
@@ -0,0 +1,95 @@
+{
+  description = "NixOS systems and tools by mitchellh";
+
+  inputs = {
+    # Pin our primary nixpkgs repository. This is the main nixpkgs repository
+    # we'll use for our configurations. Be very careful changing this because
+    # it'll impact your entire system.
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+
+    # We use the unstable nixpkgs repo for some packages.
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+
+    # Build a custom WSL installer
+    nixos-wsl.url = "github:nix-community/NixOS-WSL";
+    nixos-wsl.inputs.nixpkgs.follows = "nixpkgs";
+
+    home-manager = {
+      url = "github:nix-community/home-manager/release-23.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    darwin = {
+      url = "github:LnL7/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    # I think technically you're not supposed to override the nixpkgs
+    # used by neovim but recently I had failures if I didn't pin to my
+    # own. We can always try to remove that anytime.
+    neovim-nightly-overlay = {
+      url = "github:nix-community/neovim-nightly-overlay";
+
+      # Only need unstable until the lpeg fix hits mainline, probably
+      # not very long... can safely switch back for 23.11.
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
+    };
+
+    # Other packages
+    zig.url = "github:mitchellh/zig-overlay";
+
+    # Non-flakes
+    nvim-cinnamon.url = "github:declancm/cinnamon.nvim";
+    nvim-cinnamon.flake = false;
+    nvim-conform.url = "github:stevearc/conform.nvim/v5.2.1";
+    nvim-conform.flake = false;
+    nvim-treesitter.url = "github:nvim-treesitter/nvim-treesitter/v0.9.1";
+    nvim-treesitter.flake = false;
+    vim-copilot.url = "github:github/copilot.vim/v1.11.1";
+    vim-copilot.flake = false;
+  };
+
+  outputs = { self, nixpkgs, home-manager, darwin, ... }@inputs: let
+    # Overlays is the list of overlays we want to apply from flake inputs.
+    overlays = [
+      inputs.neovim-nightly-overlay.overlay
+      inputs.zig.overlays.default
+    ];
+
+    mkSystem = import ./lib/mksystem.nix {
+      inherit overlays nixpkgs inputs;
+    };
+  in {
+    nixosConfigurations.vm-aarch64 = mkSystem "vm-aarch64" {
+      system = "aarch64-linux";
+      user   = "mitchellh";
+    };
+
+    nixosConfigurations.vm-aarch64-prl = mkSystem "vm-aarch64-prl" rec {
+      system = "aarch64-linux";
+      user   = "mitchellh";
+    };
+
+    nixosConfigurations.vm-aarch64-utm = mkSystem "vm-aarch64-utm" rec {
+      system = "aarch64-linux";
+      user   = "mitchellh";
+    };
+
+    nixosConfigurations.vm-intel = mkSystem "vm-intel" rec {
+      system = "x86_64-linux";
+      user   = "mitchellh";
+    };
+
+    nixosConfigurations.wsl = mkSystem "wsl" {
+      system = "x86_64-linux";
+      user   = "mitchellh";
+      wsl    = true;
+    };
+
+    darwinConfigurations.macbook-pro-m1 = mkSystem "macbook-pro-m1" {
+      system = "aarch64-darwin";
+      user   = "mitchellh";
+      darwin = true;
+    };
+  };
+}
