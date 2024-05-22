5dba9cfd79adad26c02ccaebc3b4f6a1385763f6
Scott King <scott.king@sugarcrm.com>
2024-05-22 15:47:05 -0400

    Update JSON file formatting in nvim

--- users/kingscott/vim-config.nix
+++ users/kingscott/vim-config.nix
@@ -212,6 +212,7 @@ vim.cmd([[
 	autocmd FileType rust setlocal expandtab tabstop=4 shiftwidth=4 softtabstop=4
 	autocmd FileType lua setlocal expandtab tabstop=2 shiftwidth=2 softtabstop=2
 	autocmd FileType nix setlocal expandtab tabstop=2 shiftwidth=2 softtabstop=2
+	autocmd FileType json setlocal expandtab tabstop=2 shiftwidth=2 softtabstop=2
 ]])
 
 EOF
